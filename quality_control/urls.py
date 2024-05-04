9a36f95e581a369560ee89f37a33fe6a51623c32
Grigory-Shvetsov <grihon21@mail.ru>
2024-05-04 16:08:24 +0300

    Add Class-Based

--- quality_control/urls.py
+++ quality_control/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path, re_path
 from quality_control import views
 
+
 app_name = 'quality_control'
 
 urlpatterns = [
-    path('', views.index),
+    # path('', views.index),
     path('bugs/', views.bug_list, name='bug_list'),  # новый маршрут
     path('features/', views.feature_list, name='feature_list'),  # новый маршрут
     path('bugs/<int:bug_id>',views.bug_detail,),
     path('features/<int:feature_id>',views.feature_id_detail,),
+
+
+    path('',views.IndexView.as_view(), name = "index")
 ]
--- quality_control/views.py
+++ quality_control/views.py
@@ -1,11 +1,13 @@
 from django.http import HttpResponse
 from django.urls import reverse
+from django.views import View
 
-def index(request):
-    bug_list_url = reverse('quality_control:bug_list')
-    feature_list_url = reverse('quality_control:feature_list')
-    html = f"<h1>Система контроля качества</h1><a href='{bug_list_url}'>Список всех багов</a><br><a href='{feature_list_url}'>Запросы на улучшение</a>"
-    return HttpResponse(html)
+
+# def index(request):
+#     bug_list_url = reverse('quality_control:bug_list')
+#     feature_list_url = reverse('quality_control:feature_list')
+#     html = f"<h1>Система контроля качества</h1><a href='{bug_list_url}'>Список всех багов</a><br><a href='{feature_list_url}'>Запросы на улучшение</a>"
+#     return HttpResponse(html)
 
 def bug_list(request):
     html = f"<h1>Список отчетов об ошибках</h1>"
@@ -22,3 +24,10 @@ def bug_detail(request, bug_id):
 def feature_id_detail(request, feature_id):
     html = f"<p>Детали улучшения {feature_id}</p>"
     return html
+
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        bug_list_url = reverse('quality_control:bug_list')
+        feature_list_url = reverse('quality_control:feature_list')
+        html = f"<h1>Система контроля качества</h1><a href='{bug_list_url}'>Список всех багов</a><br><a href='{feature_list_url}'>Запросы на улучшение</a>"
+        return HttpResponse(html)
