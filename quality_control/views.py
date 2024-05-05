25703464a3f3cee68ef008fc05d0c4f62da77f73
Grigory-Shvetsov <grihon21@mail.ru>
2024-05-05 20:19:57 +0300

    Add FBV for quality_control

--- quality_control/views.py
+++ quality_control/views.py
@@ -5,6 +5,9 @@ from django.views import View
 from django.shortcuts import get_object_or_404
 from django.views.generic import ListView
 from django.views.generic import DetailView
+from django.template.loader import render_to_string
+from django.shortcuts import render
+
 
 
 # def index(request):
@@ -12,6 +15,14 @@ from django.views.generic import DetailView
 #     feature_list_url = reverse('quality_control:feature_list')
 #     html = f"<h1>Система контроля качества</h1><a href='{bug_list_url}'>Список всех багов</a><br><a href='{feature_list_url}'>Запросы на улучшение</a>"
 #     return HttpResponse(html)
+
+# def index(request):
+#     template = render_to_string('quality_control/index.html')
+#     return HttpResponse(template)
+
+def index(request):
+    return render(request, 'quality_control/index.html')
+
 
 # def bug_list(request):
 #     bugs = BugReport.objects.all()
@@ -22,10 +33,21 @@ from django.views.generic import DetailView
 #     return HttpResponse(bugs_html)
 
 
+
+def bug_list(request):
+    bugs = BugReport.objects.all()
+    return render(request, 'quality_control/bug_list.html', {'bug_list': bugs})
+
+
 # def bug_detail(request, bug_id):
 #     bug = get_object_or_404(BugReport, id=bug_id)
 #     response_html = f"<p>Детали бага {bug_id}:</p><a>Описание: {bug.description}</a>"
 #     return HttpResponse(response_html)
+
+
+def bug_detail(request, bug_id):
+    bug = get_object_or_404(BugReport, id=bug_id)
+    return render(request, 'quality_control/bug_detail.html', {'bug': bug})
 
 
 # def feature_list(request):
@@ -36,11 +58,22 @@ from django.views.generic import DetailView
 #     features_html += "</ul>"
 #     return HttpResponse(features_html)
 
+def feature_list(request):
+    features = FeatureRequest.objects.all()
+    return render(request, 'quality_control/feature_list.html', {'feature_list': features})
+
+
 
 # def feature_id_detail(request, feature_id):
 #     feature = get_object_or_404(BugReport, id=feature_id)
 #     response_html = f"<p>Детали улучшения {feature_id}:</p><a>Описание: {feature.description}</a>"
 #     return HttpResponse(response_html)
+
+
+def feature_id_detail(request, feature_id):
+    feature = get_object_or_404(FeatureRequest, id=feature_id)
+    return render(request, 'quality_control/feature_id_detail.html', {'feature': feature})
+
 
 
 
